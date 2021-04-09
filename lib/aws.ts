9c1c7e768b3bd84c387419e91fb4934f0ac78b9c
felixerdy <felix.erdmann@uni-muenster.de>
2021-04-09 12:54:25 +0200

    provide alt env keys for vercel

--- lib/aws.ts
+++ lib/aws.ts
@@ -3,8 +3,9 @@ import AWS from 'aws-sdk';
 const spacesEndpoint = new AWS.Endpoint(process.env.AWS_ENDPOINT);
 const s3 = new AWS.S3({
   endpoint: spacesEndpoint,
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_KEY
+  accessKeyId:
+    process.env.AWS_ACCESS_KEY_ID || process.env.AWS_ACCESS_KEY_ID_ALT,
+  secretAccessKey: process.env.AWS_SECRET_KEY || process.env.AWS_SECRET_KEY_ALT
 });
 
 export { s3 };
