b14b8fb9ed18eeeee7c6b87b45265fbcd38eb112
Matthias Pfeil <matthias.pfeil@wwu.de>
2021-07-28 13:41:30 +0200

    Add backend for deleting mobility and paper data

---
+++ pages/api/paper/[id].ts
@@ -0,0 +1,38 @@
+import prisma from '../../../lib/prisma';
+import jwt from 'next-auth/jwt';
+import { NextApiRequest, NextApiResponse } from 'next';
+
+const secret = process.env.SECRET;
+
+// DELETE /api/paper/:id
+export default async function handle(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const id = req.query.id as string;
+  const token = await jwt.getToken({ req, secret });
+
+  if (token) {
+    if (id && req.method === 'DELETE') {
+      try {
+        const result = await prisma.paperRecord.delete({
+          where: {
+            id
+          }
+        });
+
+        res.status(201).json(result);
+      } catch (error) {
+        res.status(500).json({
+          error
+        });
+      }
+    } else {
+      throw new Error(
+        `The HTTP ${req.method} method is not supported at this route.`
+      );
+    }
+  } else {
+    res.status(401).send('Unauthorized');
+  }
+}
