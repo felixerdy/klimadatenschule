21b03eb3730730369898e902feafded206da4948
Matthias Pfeil <matthias.pfeil@wwu.de>
2021-07-29 10:58:04 +0200

    Add actions to tree popup

---
+++ components/Modals/TreeModal.tsx
@@ -0,0 +1,77 @@
+import React from 'react';
+import { Modal } from '@webeetle/windy';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { toast } from 'react-toastify';
+import { useRouter } from 'next/router';
+import { TreeRecord } from '@prisma/client';
+
+interface ModalProps {
+  opened: boolean;
+  record: TreeRecord;
+  closeModal: (...args: any[]) => any;
+}
+
+const TreeModal: React.FC<ModalProps> = ({ opened, record, closeModal }) => {
+  const router = useRouter();
+
+  const { register, handleSubmit } = useForm<TreeRecord>();
+
+  const onSubmit: SubmitHandler<TreeRecord> = async data => {
+    const response = await fetch(`/api/tree/${record.id}`, {
+      method: 'POST',
+      body: JSON.stringify(data)
+    });
+
+    if (response.ok) {
+      toast.success('Datensatz erfolgreich hochgeladen');
+      router.replace(router.asPath);
+    } else {
+      toast.error(`Error: ${response.statusText}`);
+    }
+  };
+
+  return (
+    <Modal isOpen={opened} title="Baum editieren" onClickIcon={closeModal}>
+      <div>
+        <form className="p-4 max-w-xl m-auto" onSubmit={handleSubmit(onSubmit)}>
+          {record && (
+            <>
+              <div className="flex flex-col m-auto">
+                <label className="text-gray-600">Umfang in cm</label>
+                <input
+                  className="border-solid border-gray-300 border py-2 px-4 mb-4 w-full rounded text-gray-700"
+                  type="number"
+                  step="any"
+                  name={'diameter'}
+                  defaultValue={record.diameter}
+                  min={0}
+                  {...register('diameter', { min: 0 })}
+                />
+              </div>
+              <div className="flex flex-col m-auto">
+                <label className="text-gray-600">Höhe in m</label>
+                <input
+                  className="border-solid border-gray-300 border py-2 px-4 mb-4 w-full rounded text-gray-700"
+                  type="number"
+                  step="any"
+                  name={'height'}
+                  defaultValue={record.height}
+                  min={0}
+                  {...register('height', { min: 0 })}
+                />
+              </div>
+            </>
+          )}
+          <button
+            type="submit"
+            className="text-green-800 bg-green-200 px-4 py-2 mt-2  text-sm font-semibold rounded-lg hover:bg-green-100 focus:bg-blue focus:outline-none focus:shadow-outline"
+          >
+            Speichern
+          </button>
+        </form>
+      </div>
+    </Modal>
+  );
+};
+
+export default TreeModal;
--- pages/wald-baum/my.tsx
+++ pages/wald-baum/my.tsx
@@ -13,6 +13,8 @@ import { GetServerSideProps } from 'next';
 import { getSession } from 'next-auth/client';
 import prisma from '../../lib/prisma';
 import { TreeRecord } from '@prisma/client';
+import { useRouter } from 'next/router';
+import TreeModal from '../../components/Modals/TreeModal';
 
 interface TreeMarker {
   id: string;
@@ -45,6 +47,9 @@ export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
 };
 
 const WaldBaum: React.FC<{ records: TreeRecord[] }> = props => {
+  const router = useRouter();
+  const [opened, setOpened] = useState(false);
+  const [selectedRecord, setSelectedRecord] = useState<TreeRecord | null>(null);
   const [viewport, setViewport] = useState({
     width: '100%',
     height: 400,
@@ -54,6 +59,38 @@ const WaldBaum: React.FC<{ records: TreeRecord[] }> = props => {
   });
 
   const [popupInfo, setPopupInfo] = useState<TreeRecord>(null);
+
+  function closeModal() {
+    setSelectedRecord(null);
+    setOpened(false);
+  }
+
+  function openModal(record) {
+    setSelectedRecord(record);
+    setOpened(true);
+  }
+
+  const deleteRecord = async (record: TreeRecord) => {
+    try {
+      const response = await fetch(`/api/tree/${record.id}`, {
+        method: 'DELETE'
+      });
+
+      if (response.ok) {
+        toast.success('Datensatz erfolgreich gelöscht!');
+        // Refresh server side props
+        // https://www.joshwcomeau.com/nextjs/refreshing-server-side-props/
+        router.replace(router.asPath);
+
+        // Close Popup of deleted tree
+        setPopupInfo(null);
+      } else {
+        toast.error(`Error: ${response.statusText}`);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
     <Layout>
@@ -108,6 +145,41 @@ const WaldBaum: React.FC<{ records: TreeRecord[] }> = props => {
                     <p>Umfang: {popupInfo.diameter} cm</p>
                     <p>Höhe: {popupInfo.height} m</p>
                   </div>
+                  <div className="flex pt-2">
+                    <button
+                      className="m-1 text-nutrition-darkest bg-yellow-100 px-4 py-2 text-sm font-semibold rounded-lg hover:bg-yellow-200 focus:bg-gray focus:outline-none focus:shadow-outline inline-flex items-center"
+                      type="button"
+                      onClick={() => openModal(popupInfo)}
+                    >
+                      <svg
+                        className="w-4 h-4 mr-2"
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 20 20"
+                      >
+                        <path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z" />
+                      </svg>
+                      <span>Editieren</span>
+                    </button>
+                    <button
+                      className="m-1 text-nutrition-darkest bg-nutrition-lightest px-4 py-2 text-sm font-semibold rounded-lg hover:bg-nutrition-light focus:bg-gray focus:outline-none focus:shadow-outline inline-flex items-center"
+                      type="button"
+                      onClick={() => deleteRecord(popupInfo)}
+                    >
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="h-5 w-5"
+                        viewBox="0 0 20 20"
+                        fill="currentColor"
+                      >
+                        <path
+                          fillRule="evenodd"
+                          d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
+                          clipRule="evenodd"
+                        />
+                      </svg>
+                      <span>Löschen</span>
+                    </button>
+                  </div>
                 </Popup>
               )}
 
@@ -131,6 +203,13 @@ const WaldBaum: React.FC<{ records: TreeRecord[] }> = props => {
             </ReactMapGL>
           </div>
         </main>
+        {selectedRecord && (
+          <TreeModal
+            opened={opened}
+            record={selectedRecord}
+            closeModal={closeModal}
+          ></TreeModal>
+        )}
       </div>
     </Layout>
   );
