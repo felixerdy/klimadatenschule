f75c06c031469a3e5400e4a6e7011573c67948a7
felixerdy <felix.erdmann@uni-muenster.de>
2021-10-07 10:16:50 +0200

    update footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,9 +4,10 @@ import React, { useState } from 'react';
 import DatenschutzModal from './Modals/DatenschutzModal';
 import ImpressumModal from './Modals/ImpressumModal';
 
-import BildungsCentLogo from '../public/logos/bc.png';
-import MedialePfadeLogo from '../public/logos/mp_black.svg';
-import ReeduLogo from '../public/logos/reedu_black.svg';
+import BildungsCentLogo from '../public/logos/logo_bc.png';
+import MedialePfadeLogo from '../public/logos/logo_mp.png';
+import ReeduLogo from '../public/logos/logo_reedu.svg';
+import NKILogo from '../public/logos/logo_nki.png';
 
 export default function Footer() {
   const [datenschutzModalOpen, setDatenschutzModalOpen] = useState(false);
@@ -38,11 +39,16 @@ export default function Footer() {
           <div className="w-32 py-4">
             <Image src={BildungsCentLogo} alt="BildungsCent Logo"></Image>
           </div>
-          <div className="w-32">
-            <Image src={MedialePfadeLogo} alt="Mediale Pfade Logo"></Image>
+          <div className="flex items-center">
+            <div className="w-32">
+              <Image src={ReeduLogo} alt="re:edu Logo"></Image>
+            </div>
+            <div className="w-32">
+              <Image src={MedialePfadeLogo} alt="Mediale Pfade Logo"></Image>
+            </div>
           </div>
-          <div className="w-32">
-            <Image src={ReeduLogo} alt="re:edu Logo"></Image>
+          <div className="w-64 rounded overflow-hidden">
+            <Image src={NKILogo} alt="nki Logo"></Image>
           </div>
         </div>
         <div className="flex-1  mt-4">
