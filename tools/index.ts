75c0887fcb6e55ff036bf77a1ee3ac4cc286ff3a
felixerdy <felix.erdmann@uni-muenster.de>
2021-10-26 13:16:30 +0200

    add excel download

--- tools/index.ts
+++ tools/index.ts
@@ -1,4 +1,5 @@
 import { PaperProducts } from '../pages/papier';
+import { MobilityDescription, MobilityType } from '../types/mobility';
 import { PaperType } from '../types/paper';
 
 const DARRDICHTE_KG_M3 = 600;
@@ -30,4 +31,44 @@ const paperToCO2 = (gram: number, type: PaperType): number => {
   return Number(gram * PaperProducts.find(e => e.type === type).thgpst);
 };
 
-export { treeToCO2, paperToCO2 };
+// https://www.umweltbundesamt.de/themen/verkehr-laerm/emissionsdaten#grafik
+const Mobilities: MobilityDescription[] = [
+  {
+    type: 'pkw',
+    title: 'Auto in km',
+    thgpkm: 154
+  },
+  {
+    type: 'bahn',
+    title: 'Zug in km',
+    thgpkm: 54
+  },
+  {
+    type: 'bus',
+    title: 'Bus in km',
+    thgpkm: 83
+  },
+  {
+    type: 'ubahn',
+    title: 'S-Bahn / U-Bahn in km',
+    thgpkm: 54
+  },
+  {
+    type: 'fahrrad',
+    title: 'Fahrrad in km',
+    thgpkm: 0
+  },
+  {
+    type: 'fuss',
+    title: 'zu Fuß in km',
+    thgpkm: 0
+  }
+];
+
+const mobilityToCO2 = (distanceInKm: number, type: MobilityType): number => {
+  return Number(
+    (distanceInKm / 1000) * Mobilities.find(e => e.type === type).thgpkm
+  );
+};
+
+export { treeToCO2, paperToCO2, mobilityToCO2 };
